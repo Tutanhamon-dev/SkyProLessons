c1e9e354ead5e0c31dc19d21e17e459a460c7816
Tutanhamon <ramsses82@gmail.com>
2025-10-15 20:03:42 +0300

    Переименование переменных в mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(original_string: str) -> str:
+def mask_account_card(original_text_string: str) -> str:
     """Принимает строку с названим карты и номером или номером счета и возвращает сокрытый номер"""
-    original_string_list = original_string.split()
-    if len(original_string_list[-1]) < 20:
-        original_string_list[-1] = get_mask_card_number(original_string_list[-1])
+    original_text_list = original_text_string.split()
+    if len(original_text_list[-1]) < 20:
+        original_text_list[-1] = get_mask_card_number(original_text_list[-1])
     else:
-        original_string_list[-1] = get_mask_account(original_string_list[-1])
-    return " ".join(original_string_list)
+        original_text_list[-1] = get_mask_account(original_text_list[-1])
+    return " ".join(original_text_list)
 
 
 def get_date(full_date: str) -> str:
