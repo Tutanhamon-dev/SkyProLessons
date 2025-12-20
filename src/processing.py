9b663c3fd3be05d911856272ace4d76637a0951b
Tutanhamon-dev <ramsses82@gmail.com>
2025-12-20 20:13:45 +0300

    Исправлен ключ сортровки в sort_by_date, использован модуль typing для анотации типов

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,16 @@
-def filter_by_state(dictionary_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+from typing import List, Dict
+
+
+def filter_by_state(dictionary_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """Возвращает список словарей по state"""
-    filtered_dictionary_list: list[dict] = []
+    filtered_dictionary_list: List[Dict] = []
     for dictionary in dictionary_list:
         if dictionary["state"] == state:
             filtered_dictionary_list.append(dictionary)
     return filtered_dictionary_list
 
 
-def sort_by_date(dictionary_list: list[dict], direction: bool = True) -> list[dict]:
+def sort_by_date(dictionary_list: List[Dict], direction: bool = True) -> List[Dict]:
     """Возвращает список словарей отсортированный по date"""
-    sorted_dictionary_list = sorted(dictionary_list, key=lambda x: ["date"], reverse=direction)
+    sorted_dictionary_list = sorted(dictionary_list, key=lambda x: "date", reverse=direction)
     return sorted_dictionary_list
